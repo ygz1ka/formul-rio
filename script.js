dc81b5937ba990e95e2f48f8177ef04710d67c41
ygz1ka <ydiasrafa4@gmail.com>
2026-03-26 13:07:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function Clicar(){
+    Swal.fire({
+  title: "Cadastro Concluido!",
+  text: "Volte para a Página de login e acesse o site!",
+  icon: "success"
+});
+}
+function Apertar(){
+     Swal.fire({
+  title: "Login Concluido!",
+  text: "Acesse o site!",
+  icon: "success"
+});
+}
